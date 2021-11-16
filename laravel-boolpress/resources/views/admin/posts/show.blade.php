1713bc3159dc5bff5a0153d654908abacf571b8a
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-11-16 16:16:26 +0100

    Aggiornata la admin.show e termine esercizio

--- laravel-boolpress/resources/views/admin/posts/show.blade.php
+++ laravel-boolpress/resources/views/admin/posts/show.blade.php
@@ -12,6 +12,11 @@
             </div>  
                 
             <div class="col-10">
+
+                <p><strong>Categoria : </strong> 
+                    @if ($post->category) {{ $post->category->name }} @else Nessuna categoria. 
+                    @endif
+                </p>
                 <p><strong>Autore : </strong> {{ $post->author }}</p>
                 <p><strong>Scritto il:</strong> {{ $post->date }}</p>
                 <p><strong>Contenuto : </strong> {{ $post->content }}</p>
